0b149e9b35de285deaee10faaf62df48c681f850
jengdeuk <jengdeuk@gmail.com>
2026-05-11 21:45:22 +0900

    [feat_Resonator] 대시, 기본 공격 추가

--- Source/WuWa/Character/Resonator/Resonator.cpp
+++ Source/WuWa/Character/Resonator/Resonator.cpp
@@ -1,6 +1,9 @@
 #include "Character/Resonator/Resonator.h"
+#include "DataAsset/AttackComboData.h"
+
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
 #include "InputTriggers.h"
@@ -14,7 +17,6 @@ AResonator::AResonator()
 	bUseControllerRotationRoll = false;
 
 	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
 	GetCharacterMovement()->JumpZVelocity = 600.0f;
 	GetCharacterMovement()->GravityScale = 1.5f;
@@ -25,9 +27,38 @@ AResonator::AResonator()
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->TargetArmLength = 600.0f;
 	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bEnableCameraRotationLag = true;
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), TEXT("WeaponProp05"));
+	Weapon->SetVisibility(false);
+
+	SetCurrentState(EResonatorState::Normal);
+	SetCurrentLocomotionGait(ELocomotionGait::Run);
+}
+
+void AResonator::SetCurrentState(const EResonatorState NextState)
+{
+	CurrentState = NextState;
+}
+
+void AResonator::SetCurrentLocomotionGait(const ELocomotionGait NextLocomotionGait)
+{
+	switch (NextLocomotionGait)
+	{
+	case ELocomotionGait::Run:
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+		break;
+	case ELocomotionGait::Sprint:
+		GetCharacterMovement()->MaxWalkSpeed = 750.0f;
+		break;
+	}
+
+	CurrentLocomotionGait = NextLocomotionGait;
 }
 
 void AResonator::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -40,6 +71,26 @@ void AResonator::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AResonator::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AResonator::Look);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AResonator::Jump);
+		EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Started, this, &AResonator::Dash);
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &AResonator::Attack);
+	}
+}
+
+void AResonator::Tick(float DeltaSeconds)
+{
+	TickLocomotionGait(DeltaSeconds);
+}
+
+void AResonator::TickLocomotionGait(float DeltaSeconds)
+{
+	switch (CurrentLocomotionGait)
+	{
+	case ELocomotionGait::Sprint:
+		if (GetVelocity().Length() <= 50.0f)
+		{
+			SetCurrentLocomotionGait(ELocomotionGait::Run);
+		}
+		break;
 	}
 }
 
@@ -53,8 +104,16 @@ void AResonator::Move(const FInputActionValue& Value)
 	FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 	FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
-	AddMovementInput(ForwardVector, Movement.Y);
-	AddMovementInput(RightVector, Movement.X);
+	FVector NewMoveInputDirection = ForwardVector * Movement.Y + RightVector * Movement.X;
+	if (NewMoveInputDirection.IsNearlyZero())
+	{
+		return;
+	}
+
+	CurrentMoveInputDirection = NewMoveInputDirection.GetSafeNormal();
+	AddMovementInput(CurrentMoveInputDirection);
+
+	OnAnyGameplayInput();
 }
 
 void AResonator::Look(const FInputActionValue& Value)
@@ -67,17 +126,193 @@ void AResonator::Look(const FInputActionValue& Value)
 
 void AResonator::Jump()
 {
+	if (CurrentState != EResonatorState::Normal)
+	{
+		return;
+	}
+
 	if (GetMovementComponent()->IsFalling() || GetMovementComponent()->IsFlying())
 	{
 		return;
 	}
 
-	if (GetVelocity().Length() > 0)
+	OnAnyGameplayInput();
+
+	if (GetVelocity().Length() > 50.0f)
 	{
-		PlayAnimMontage(JumpRunMontage);
+		PlayAnimMontage(JumpRunMontage, 1.5f);
 	}
 	else
 	{
 		PlayAnimMontage(JumpMontage, 1.5f);
 	}
+}
+
+void AResonator::Dash()
+{
+	if (CurrentState != EResonatorState::Normal)
+	{
+		return;
+	}
+
+	if (CurrentLocomotionGait == ELocomotionGait::Dash)
+	{
+		return;
+	}
+
+	OnAnyGameplayInput();
+
+	SetCurrentLocomotionGait(ELocomotionGait::Dash);
+	SetRotationByMoveInput();
+
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	FOnMontageEnded EndDelegate;
+	EndDelegate.BindUObject(this, &AResonator::OnDashMontageEnded);
+	if (GetMovementComponent()->IsFalling() || GetMovementComponent()->IsFlying())
+	{
+		PlayAnimMontage(JumpDashMontage, 1.5f);
+		AnimInstance->Montage_SetEndDelegate(EndDelegate, JumpDashMontage);
+	}
+	else if (GetVelocity().Length() > 50.0f)
+	{
+		PlayAnimMontage(DashMontage, 1.5f);
+		AnimInstance->Montage_SetEndDelegate(EndDelegate, DashMontage);
+	}
+	else
+	{
+		PlayAnimMontage(DashMontage, 1.5f);
+		AnimInstance->Montage_SetEndDelegate(EndDelegate, DashMontage);
+		AnimInstance->Montage_JumpToSection(TEXT("Back"), DashMontage);
+	}
+}
+
+void AResonator::OnAnyGameplayInput()
+{
+	if (!bCanCancelAttack)
+	{
+		return;
+	}
+
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (AnimInstance && AttackMontage)
+	{
+		AnimInstance->Montage_Stop(0.15f, AttackMontage);
+	}
+}
+
+void AResonator::OnDashMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	SetCurrentLocomotionGait(ELocomotionGait::Sprint);
+}
+
+void AResonator::Attack()
+{
+	OnAnyGameplayInput();
+
+	if (CurrentAttackCombo == 0)
+	{
+		BeginComboAttack();
+		return;
+	}
+
+	if (AttackComboTimer.IsValid())
+	{
+		bHasNextComboCommand = true;
+	}
+	else
+	{
+		bHasNextComboCommand = false;
+	}
+}
+
+void AResonator::BeginComboAttack()
+{
+	CurrentAttackCombo = 1;
+	SetCurrentState(EResonatorState::Attack);
+	SetRotationByMoveInput();
+
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	const float AttackSpeedRate = 1.5f;
+	AnimInstance->Montage_Play(AttackMontage, AttackSpeedRate);
+
+	FOnMontageEnded EndDelegate;
+	EndDelegate.BindUObject(this, &AResonator::EndComboAttack);
+	AnimInstance->Montage_SetEndDelegate(EndDelegate, AttackMontage);
+
+	SetAttackComboTimer();
+}
+
+void AResonator::EndComboAttack(UAnimMontage* TargetMontage, bool bInterrupted)
+{
+	CurrentAttackCombo = 0;
+	SetCurrentState(EResonatorState::Normal);
+}
+
+void AResonator::SetAttackComboTimer()
+{
+	AttackComboTimer.Invalidate();
+
+	const int32 ComboIndex = CurrentAttackCombo - 1;
+	ensureAlways(AttackComboData->EffectiveFrameCount.IsValidIndex(ComboIndex));
+
+	const float AttackSpeedRate = 1.5f;
+	float ComboEffectTime = (AttackComboData->EffectiveFrameCount[ComboIndex] / AttackComboData->FrameRate / AttackSpeedRate);
+	if (ComboEffectTime > 0)
+	{
+		GetWorld()->GetTimerManager().SetTimer(AttackComboTimer, this, &AResonator::CheckAttackComboInput, ComboEffectTime, false);
+	}
+}
+
+void AResonator::CheckAttackComboInput()
+{
+	AttackComboTimer.Invalidate();
+	if (!bHasNextComboCommand)
+	{
+		return;
+	}
+
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	CurrentAttackCombo = FMath::Clamp(CurrentAttackCombo + 1, 1, AttackComboData->MaxComboCount);
+	SetCurrentState(EResonatorState::Attack);
+	SetRotationByMoveInput();
+
+	FName NextSection = *FString::Printf(TEXT("%s%d"), *AttackComboData->MontageSectionNamePrefix, CurrentAttackCombo);
+
+	AnimInstance->Montage_JumpToSection(NextSection, AttackMontage);
+
+	SetAttackComboTimer();
+
+	bHasNextComboCommand = false;
+}
+
+void AResonator::SetRotationByMoveInput()
+{
+	const FVector PrevCamLoc = Camera->GetComponentLocation();
+	const FRotator PrevCamRot = Camera->GetComponentRotation();
+
+	SetActorRotation(CurrentMoveInputDirection.Rotation());
+
+	//const FVector NewCamLoc = Camera->GetComponentLocation();
+	//const FVector Delta = PrevCamLoc - NewCamLoc;
+	//SpringArm->SocketOffset += SpringArm->GetComponentTransform().InverseTransformVectorNoScale(Delta);
+	//
+	//if (Controller)
+	//{
+	//	Controller->SetControlRotation(PrevCamRot);
+	//}
 }
