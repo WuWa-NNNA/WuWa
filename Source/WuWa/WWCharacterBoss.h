84c43a9a8456b792aa0d7b63c6f4e1f8812aadf3
lelana0824 <lelana0824@gmail.com>
2026-05-11 10:56:05 +0900

    시길룸 블루프린트 부모 재설정 - WWCharacterBoss로 변경

---
+++ Source/WuWa/WWCharacterBoss.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WWCharacter.h"
+#include "WWCharacterBoss.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WUWA_API AWWCharacterBoss : public AWWCharacter
+{
+	GENERATED_BODY()
+	
+};
