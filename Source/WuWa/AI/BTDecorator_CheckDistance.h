47d05652380c0e779a19cbebed16ab59a2c5878d
lelana0824 <lelana0824@gmail.com>
2026-05-14 12:44:22 +0900

    [feat_Sigillum] BT 데코레이터 생성 및 적용

---
+++ Source/WuWa/AI/BTDecorator_CheckDistance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "EDistanceRange.h"
+
+#include "BTDecorator_CheckDistance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WUWA_API UBTDecorator_CheckDistance : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+    UBTDecorator_CheckDistance();
+
+protected:
+    virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+    UPROPERTY(EditAnywhere, Category = "Condition")
+    TArray<EDistanceRange> AllowedDistances;
+
+    UPROPERTY(EditAnywhere, Category = "Condition")
+    struct FBlackboardKeySelector DistanceKey;
+};
